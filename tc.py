b964ee3dd5d4a0c150ef011cbcfd8a13db150f86
Froglingexe <138542096+Froglingexe@users.noreply.github.com>
2023-07-05 06:47:48 +0300

    tc.py

--- tc.py
+++ tc.py
@@ -1,42 +1,60 @@
 import mysql.connector
 
-
-db = mysql.connector.connect(
+# Veritabanı bağlantısı için gerekli bilgileri girin
+mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="",
     database="101m"
 )
 
+# Cursor oluştur
+mycursor = mydb.cursor()
 
-if db.is_connected():
-    print("Veritabanına başarıyla bağlandı.")
+# Kullanıcıdan girdileri al
+tc = input("Sorgulanacak kişinin TC'sini giriniz: ")
 
-tc = input("Lütfen TC kimlik numarasını girin: ")
+# Verilerin yazılacağı dosyanın adını oluştur
+dosya_adi = tc + ".txt"
 
-
-cursor = db.cursor()
-cursor.execute(f"SELECT * FROM 101m WHERE TC = '{tc}'")
-result = cursor.fetchone()
-
-
-with open("tcsorgu.txt", "a", encoding="utf-8") as f:
-    if result:
-        adi = result[2]
-        soyadi = result[3]
-        dogum_tarihi = result[4]
-        nufus_yeri = result[5]
-        ilce = result[6]
-        anne_adi = result[7]
-        anne_tc = result[8]
-        baba_adi = result[9]
-        baba_tc = result[10]
-        uyruk = result[11]
-        f.write(f"TC: {tc} ADI: {adi}  SOYADI: {soyadi} DOĞUM TARİHİ: {dogum_tarihi} NÜFUS İL: {nufus_yeri} NÜFÜS İLÇE: {ilce} ANNE ADI: {anne_adi} ANNE TC: {anne_tc} BABA ADI: {baba_adi} BABA TC: {baba_tc} UYRUK: {uyruk}\n")
-        print(f"TC: {tc} ADI: {adi}  SOYADI: {soyadi} DOĞUM TARİHİ: {dogum_tarihi} NÜFUS İL: {nufus_yeri} NÜFÜS İLÇE: {ilce} ANNE ADI: {anne_adi} ANNE TC: {anne_tc} BABA ADI: {baba_adi} BABA TC: {baba_tc} UYRUK: {uyruk}")
+# Verileri veritabanında ara ve dosyaya yaz
+try:
+    if tc:
+        sql = f"SELECT * FROM 101m WHERE TC = '{tc}'"
     else:
-        f.write(f"{tc} için sonuç bulunamadı.\n")
-        print(f"{tc} için sonuç bulunamadı.")
+        sql = f"SELECT * FROM 101m WHERE TC = '{tc}'"
 
-# Bağlantıyı kapat
-db.close()
+
+
+    # Sorguyu çalıştır
+    mycursor.execute(sql)
+
+    # Sonuçları dosyaya yaz
+    with open(dosya_adi, "wb") as dosya:
+        for kayit in mycursor:
+            tc = kayit[1]
+            adi = kayit[2]
+            soyadi = kayit[3]
+            dogum_tarihi = kayit[4] 
+            nufus_il = kayit[5]
+            nufus_ilce = kayit[6]
+            ana_adi = kayit[7]           
+            ana_tc = kayit[8]           
+            baba_adi = kayit[9]
+            baba_tc = kayit[10]
+            uyruk = kayit[11]
+
+            
+            dosya.write(f"TC:{tc}, ADI:{adi}, SOYADI:{soyadi}, DOĞUM TARIHI:{dogum_tarihi}, NUFUS İL:{nufus_il}, NUFUS İLÇE:{nufus_ilce}"
+                        f"ANNE ADI:{ana_adi}, ANNE TC:{ana_tc}, BABA ADI:{baba_adi}, BABA TC:{baba_tc}, UYRUK:{uyruk}\n".encode())
+            
+
+            print(f"TC:{tc}, ADI:{adi}, SOYADI:{soyadi}, DOĞUM TARIHI:{dogum_tarihi}, NUFUS İL:{nufus_il}, NUFUS İLÇE:{nufus_ilce}"
+                        f"ANNE ADI:{ana_adi}, ANNE TC:{ana_tc}, BABA ADI:{baba_adi}, BABA TC:{baba_tc}, UYRUK:{uyruk}\n".encode())  
+
+    print(f"Veriler {dosya_adi} dosyasına kaydedildi.")
+    
+except mysql.connector.errors.ProgrammingError:
+    print("Hata: Geçersiz sorgu.")
+except Exception as e:
+    print("Hata:", e)
